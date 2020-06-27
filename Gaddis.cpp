700bd5e90222dadc57ab9a90fc50cd97812653b0
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-06-27 16:17:59 +0530

    Create Gaddis.cpp

---
+++ Gaddis.cpp
@@ -0,0 +1,18 @@
+//This program calculates the user's pay. 
+#include<iostream>
+using namespace std;
+int main()
+{
+double hours ,rate ,pay ;
+//Get the number of hours worked .
+Cout << "How many hours did you work ?";
+cin >> hours ;
+//Get the hours pay rate.
+cout << "How much do you get paid per hour ?";
+cin>> rate ;
+//Calculate the pay .
+pay =hours * rate ;
+//Display the pay 
+cout << "You have earned $ "  << pay << end 1;
+return 0;
+}
